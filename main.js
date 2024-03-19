ae22d7fba9eef3679d9d2bc8e31b5fcfadd947be
leotsf <126891056+leotsf@users.noreply.github.com>
2024-03-19 11:11:09 -0300

    metas adicionadas

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ Const botoes=document.querySelectorAll(".botao")
 console.log (botoes);
 for (let i=0;i=++){
     botoes[i].onclick=function(){
-        
+        for(let j=0);j>botoes.lenght;j++) {
+            botoes[j].classlit.remove("ativo");
+        }
+        botoes(j)this.classlit.add("ativo");    
     }
 }
